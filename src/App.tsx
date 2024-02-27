8c2287ee4f18433bf7892acbb95d1092f3254551
li-yu-tfs <yu.li2@thermofisher.com>
2024-02-27 11:33:20 +0800

    Add drop3 menu item

--- src/App.tsx
+++ src/App.tsx
@@ -14,6 +14,7 @@ function App() {
           <Menu.Item>drop2</Menu.Item>
           <Menu.Item>drop3</Menu.Item>
           <Menu.Item>drop3</Menu.Item>
+          <Menu.Item>drop3</Menu.Item>
         </Menu.SubMenu>
       </Menu>
     </div>
